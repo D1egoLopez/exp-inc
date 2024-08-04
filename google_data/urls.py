b7b1a45c0bf71ba8cff4fc4d6e6a635392e3ced5
Diego  Lopez <67641488+D1egoLopez@users.noreply.github.com>
2024-08-04 12:22:32 -0300

    Delete google_data directory
    .

--- google_data/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('raw-data-sheet-exp', views.exp, name='expenses data'),
-    path('raw-data-sheet-inc', views.inc, name='income data'),
-    path('sumas', views.summas )
-]
